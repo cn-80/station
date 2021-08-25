b3efe0b9750f1608b4063f1790afff887a4f72da
sim <sim@terra.money>
2021-08-25 20:34:13 +0900

    wip: checkbox

--- src/pages/bank/SearchToken.tsx
+++ src/pages/bank/SearchToken.tsx
@@ -1,22 +1,27 @@
 import { AccAddress } from '@terra-money/terra.js'
+import { without } from 'ramda'
 import { useState } from 'react'
+import Checkbox from '../../components/Checkbox'
 import { Token, Whitelist } from '../../lib'
 import styles from './SearchToken.module.scss'
 
 const TokenItem = ({ token, symbol, decimals, icon }: Token) => (
-  <article className={styles.item}>
+  <article className={styles.token}>
     <img src={icon} alt="" width={32} height={32} />
 
     <section className={styles.details}>
       <h1>{symbol}</h1>
-      <small>{token}</small>
-      {decimals && <p>{decimals}</p>}
+      <small>
+        {token} (decimals: {decimals ?? '6'})
+      </small>
     </section>
   </article>
 )
 
 const SearchToken = ({ whitelist }: { whitelist: Whitelist }) => {
   const [input, setInput] = useState('')
+  const [checked, setChecked] = useState<string[]>([])
+
   const filter = ([token, { symbol }]: [string, Token]) =>
     symbol.toLowerCase().includes(input.toLowerCase()) ||
     (AccAddress.validate(input) && token.includes(input))
@@ -27,11 +32,22 @@ const SearchToken = ({ whitelist }: { whitelist: Whitelist }) => {
       <ul className={styles.list}>
         {Object.entries(whitelist)
           .filter(filter)
-          .map(([address, token]) => (
-            <li key={address}>
-              <TokenItem {...token} />
-            </li>
-          ))}
+          .map(([address, token]) => {
+            const isChecked = checked.includes(address)
+            const handleClick = () => {
+              setChecked((checked) =>
+                isChecked ? without([address], checked) : [...checked, address]
+              )
+            }
+
+            return (
+              <li className={styles.item} key={address}>
+                <Checkbox onClick={handleClick} checked={isChecked}>
+                  <TokenItem {...token} />
+                </Checkbox>
+              </li>
+            )
+          })}
       </ul>
     </>
   )
